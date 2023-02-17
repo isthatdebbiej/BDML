a861aca43fd6b96838f1de0ce28991afd354581d
Deborah Shekinah Jacob <dshekinah.93@gmail.com>
2023-02-17 13:13:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template
-import os
+from random import randint
+from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route("/rolldice")
+def roll_dice():
+    return str(do_roll())
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+def do_roll():
+    return randint(1, 6)
