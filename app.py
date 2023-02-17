131fa01b4409f5ef826e4580041d916a05a466a2
Deborah Shekinah Jacob <dshekinah.93@gmail.com>
2023-02-17 13:03:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
-# These are the necessary import declarations
-from opentelemetry import trace
-
-from random import randint
-from flask import Flask, request
-
-# Acquire a tracer
-tracer = trace.get_tracer(__name__)
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route("/rolldice")
-def roll_dice():
-    return str(do_roll())
 
-def do_roll():
-    # This creates a new span that's the child of the current one
-    with tracer.start_as_current_span("do_roll") as rollspan:  
-        res = randint(1, 6)
-        rollspan.set_attribute("roll.value", res)
-        return res
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
